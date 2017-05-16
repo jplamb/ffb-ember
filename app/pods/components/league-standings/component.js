6fec1cd299b8104d9b4968438f1a6d8bd0eaa53e
John Lamb <j.lamb13@gmail.com>
2017-05-15 21:54:19 -0500

    added def points and built out weekly standings results

--- app/pods/components/league-standings/component.js
+++ app/pods/components/league-standings/component.js
@@ -8,30 +8,51 @@ export default Ember.Component.extend({
 	weeks: ['Week 1', 'Week 2', 'Week 3', 'Week 4', 'Week 5', 'Week 6', 'Week 7', 'Week 8', 'Week 9', 'Week 10'],
 	weekSelected: 'Week 1',
 
-	rawStandings: Ember.computed('weekSelected', function() {
-		let week = this.get('weekSelected');
+	rawStandings: Ember.computed(function() {
+		return this.get('store').findAll('team-point');
+	}),
 
-		if (!week) {
-			week = 1;
-		} else {
-			week = parseInt(week.split(' ')[1]);
-		}
-		return this.get('store').query('team-point', {week:week});
+	rawDefStandings: Ember.computed(function() {
+		return this.get('store').findAll('team-def-point');
 	}),
 
 	ob: Ember.observer('rawStandings', 'rawStandings.isFulfilled', function() {
 		console.log(this.get('rawStandings'));
 	}),
 
-	standings: Ember.computed('rawStandings', 'rawStandings.isFulfilled', function() {
-		let points = this.get('rawStandings');
+	standings: Ember.computed('rawStandings.isFulfilled', 'rawDefStandings.isFulfilled', function() {
+		const points = this.get('rawStandings');
+		const defPoints = this.get('rawDefStandings');
 
-		let standings = {};
+		if (!points.isFulfilled || !defPoints.isFulfilled) {return;}
 
-		if (!points.isFulfilled) {return;}
+		return this.calculateStandings([...points.toArray(), ...defPoints.toArray()]);
+	}),
+
+	weekStandings: Ember.computed('weekSelected', 'rawStandings.isFulfilled', 'rawDefStandings.isFulfilled', function() {
+		let week = this.get('weekSelected');
+		const rawStandings = this.get('rawStandings');
+		const rawDefStandings = this.get('rawDefStandings');
+
+		if (!rawStandings.isFulfilled || !rawDefStandings.isFulfilled) {return;}
+
+		if (!week) {
+			week = 1;
+		} else {
+			week = parseInt(week.split(' ')[1]);
+		}
+
+		const weeklyStandings = rawStandings.filterBy('week', week);
+		const weeklyDefStandings = rawDefStandings.filterBy('week', week);
+
+		return this.calculateStandings([...weeklyStandings, ...weeklyDefStandings]);
+	}),
+
+	calculateStandings(points) {
+		const standings = {};
 
 		points.forEach(player => {
-			if (player.get('fant_team')) {
+			if (player.get('fant_team') && player.get('points_elig') === 'Y' && player.get('is_starting') === 'Y') {
 				if (!(player.get('fant_team') in standings)) {
 					standings[player.get('fant_team')] = player.get('total_points');
 				} else {
@@ -49,9 +70,9 @@ export default Ember.Component.extend({
 		sortedStandings.sort((a,b) => {
 			return b[1] - a[1];
 		});
-		console.log(sortedStandings);
+
 		return sortedStandings;
-	}),
+	}
 
 });
 
